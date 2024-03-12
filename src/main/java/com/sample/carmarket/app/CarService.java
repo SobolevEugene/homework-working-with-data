b21f15448b435aebb5f4df9207620df5ef544c77
SobolevEA <SobolevEA@dgtserv.ru>
2024-03-12 10:46:40 +0300

    4. Изменение данных с помощью DataManager

--- src/main/java/com/sample/carmarket/app/CarService.java
+++ src/main/java/com/sample/carmarket/app/CarService.java
@@ -1,10 +1,12 @@
 package com.sample.carmarket.app;
 
 import com.sample.carmarket.entity.Car;
+import com.sample.carmarket.entity.CarStatus;
 import com.sample.carmarket.entity.Manufacturer;
 import io.jmix.core.DataManager;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,6 +27,13 @@ public class CarService {
 				.query("select c from Car c where c.model.manufacturer.id = :manufacturerId")
 				.parameter("manufacturerId", manufacturer.getId())
 				.list();
-
 	}
+
+	public void markCarAsSold(Car car) {
+		car.setStatus(CarStatus.SOLD);
+		car.setSaleDate(LocalDate.now());
+		dataManager.save(car);
+	}
+
+
 }
--- src/main/java/com/sample/carmarket/screen/car/CarBrowse.java
+++ src/main/java/com/sample/carmarket/screen/car/CarBrowse.java
@@ -1,10 +1,39 @@
 package com.sample.carmarket.screen.car;
 
+import com.sample.carmarket.app.CarService;
+import com.sample.carmarket.entity.CarStatus;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.screen.*;
 import com.sample.carmarket.entity.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Car.browse")
 @UiDescriptor("car-browse.xml")
 @LookupComponent("carsTable")
 public class CarBrowse extends StandardLookup<Car> {
+	@Autowired
+	private GroupTable<Car> carsTable;
+	@Autowired
+	private Notifications notifications;
+	@Autowired
+	private CarService carService;
+
+	@Subscribe("carsTable.markAsSold")
+	public void onCarsTableMarkAsSold(final Action.ActionPerformedEvent event) {
+		var selectedCar = carsTable.getSingleSelected();
+		if (selectedCar==null) {
+			return;
+		}
+
+		if (selectedCar.getStatus().equals(CarStatus.SOLD)) {
+			notifications.create().withCaption("Already Sold").show();
+		}
+
+		if (selectedCar.getStatus().equals(CarStatus.IN_STOCK)) {
+			carService.markCarAsSold(selectedCar);
+			notifications.create().withCaption("Done").show();
+		}
+	}
 }
